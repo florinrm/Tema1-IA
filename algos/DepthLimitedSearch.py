579448cb2b495d1432ccb8c928c5024e04f3d8db
Florin <florin.razvan.mihalache@gmail.com>
2019-11-05 23:44:38 +0200

    Added visited list to UCS and DLS

--- algos/DepthLimitedSearch.py
+++ algos/DepthLimitedSearch.py
@@ -3,14 +3,18 @@ from Driver import Driver
 def DepthLimitedSearch(state, budget, k):
     open = [state]
     state.parent = None
+    visited = []
     while len(open) > 0:
         current = open.pop(0)
+        visited.append((current.x, current.y))
         if current.goal():
             current.fuel = current.fuel + budget
             return current
         if current.depth >= k:
             continue
         for succ in current.succesors():
+            if ((succ.x, succ.y) in visited):
+                continue
             succ.parent = current
             open.insert(0, succ)
     return False
--- algos/UniformCostSearch.py
+++ algos/UniformCostSearch.py
@@ -4,12 +4,16 @@ from algos.utils import g
 def UniformCostSearch(state, budget):
     open = [state]
     state.parent = None
+    visited = []
     while len(open) > 0:
         current = open.pop(0)
+        visited.append((current.x, current.y))
         if current.goal():
             current.fuel = current.fuel + budget
             return current
         for succ in current.succesors():
+            if ((succ.x, succ.y) in visited):
+                continue
             succ.parent = current
             succ.costFromInitial = g(succ)
             open.append(succ)
